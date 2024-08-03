9b1575561b864a2c510fac361abded66ef04485e
GAV <GAV@d.c>
2024-08-03 20:44:59 +0300

    finish intro xml

---
+++ app/src/main/java/dev/androidbroadcast/onlineshopapp/IntroActivity.kt
@@ -0,0 +1,20 @@
+package dev.androidbroadcast.onlineshopapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.androidbroadcast.onlineshopapp.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
